a4767d44be7d5b5d19872132c3231894ded4bb08
B-C-E <syanik1236@student.whatcom.edu>
2019-09-29 20:24:43 -0700

    added code outline

---
+++ src/com/ourGroup/Main.java
@@ -0,0 +1,53 @@
+// Students: Alex Wesolowki, Chantel Drwg, Sean Yanik
+// Class: CS 145 A with Mr. Criss
+// Lab 1
+/*
+This program invites the user to play a guessing game. The computer picks a random number
+from a range and the user tries to guess it, while being told if their guesses are too low
+or too high.
+Includes adjustable difficulty
+ */
+
+package com.ourGroup;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	// write your code here
+    }
+
+    public static void setupDifficulty()
+    {
+
+    }
+
+
+    public static boolean getInput()
+    {
+
+    }
+
+    public static int getInput(int minRange, int maxRange)
+    {
+
+    }
+
+    public static boolean getInput(String option1, String option2, String option3)
+    {
+
+    }
+
+    public static void gameLoop()
+    {
+
+    }
+
+    public static void doGuess(int minRange, int maxRange, int guessesLeft, int guessesMade)
+    {
+
+    }
+
+
+}
